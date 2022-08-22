aa38feb085e9a8a680b6ba16f614e149f5f4a5b6
Leigh <leigh@ivts.co.uk>
2022-08-22 11:56:38 +0100

    Add project files.

---
+++ MauiApp1/MauiProgram.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Maui.LifecycleEvents;
+using CommunityToolkit.Maui;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Compatibility;
+using Microsoft.Maui.Controls.Xaml;
+
+namespace MauiApp1;
+
+#if WINDOWS
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Windows.Graphics;
+#endif
+
+public static class MauiProgram {
+    public static MauiApp CreateMauiApp() {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts => {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            }).UseMauiCommunityToolkit();
+
+        // https://stackoverflow.com/a/73323471/513450
+//#if WINDOWS
+//        builder.ConfigureLifecycleEvents(events =>
+//        {
+//            events.AddWindows(wndLifeCycleBuilder =>
+//            {
+//                wndLifeCycleBuilder.OnWindowCreated(window =>
+//                {
+//                    IntPtr nativeWindowHandle = WinRT.Interop.WindowNative.GetWindowHandle(window);
+                   
+//                    WindowId win32WindowsId = Win32Interop.GetWindowIdFromWindow(nativeWindowHandle);
+
+//                    AppWindow winuiAppWindow = AppWindow.GetFromWindowId(win32WindowsId);    
+//                    winuiAppWindow.TitleBar.ExtendsContentIntoTitleBar = true;
+//                    winuiAppWindow.TitleBar.BackgroundColor = Windows.UI.Color.FromArgb(255, 120,120,120);
+//                    winuiAppWindow.TitleBar.PreferredHeightOption = TitleBarHeightOption.Standard;
+
+//                    if(winuiAppWindow.Presenter is OverlappedPresenter p) { 
+//                       p.Maximize();
+//                       p.IsAlwaysOnTop=true;
+//                       p.IsResizable=false;
+//                       p.IsMaximizable = false;
+//                       p.IsMinimizable=false;
+                      
+//                       }                     
+//                    else
+//                    {
+//                        const int width = 1920;
+//                        const int height = 1080;
+//                        winuiAppWindow.MoveAndResize(new RectInt32(1920 / 2 - width / 2, 1080 / 2 - height / 2, width, height));                      
+//                        }                        
+//                });
+//            });
+//        });
+//#endif
+
+        return builder.Build();
+    }
+}
---
+++ MauiApp1/Platforms/Windows/App.xaml.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Microsoft.UI.Xaml;
+using Windows.Graphics;
+using Windows.UI.ViewManagement;
+using WinRT.Interop;
+using static PInvoke.User32;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace MauiApp1.WinUI;
+
+/// <summary>
+/// Provides application-specific behavior to supplement the default Application class.
+/// </summary>
+public partial class App : MauiWinUIApplication
+{
+	/// <summary>
+	/// Initializes the singleton application object.  This is the first line of authored code
+	/// executed, and as such is the logical equivalent of main() or WinMain().
+	/// </summary>
+	public App()
+	{
+		this.InitializeComponent();
+    }
+
+	protected override void OnLaunched(LaunchActivatedEventArgs args) {
+		base.OnLaunched(args);
+
+
+        var currentWindow = Application.Windows[0].Handler.PlatformView;
+        IntPtr _windowHandle = WindowNative.GetWindowHandle(currentWindow);
+        var windowId = Win32Interop.GetWindowIdFromWindow(_windowHandle);
+
+        //// https://docs.microsoft.com/en-us/windows/apps/winui/winui3/desktop-winui3-app-with-basic-interop
+        //SetWindowDetails(_windowHandle, 1920, 1080);
+        //m_window.Activate();
+       
+        AppWindow appWindow = AppWindow.GetFromWindowId(windowId);
+        appWindow.TitleBar.ExtendsContentIntoTitleBar = false;
+
+        //appWindow.Resize(new SizeInt32(1920, 1080));
+        //
+        appWindow.SetPresenter(AppWindowPresenterKind.FullScreen);
+    }
+
+    private static void SetWindowDetails(IntPtr hwnd, int width, int height) {
+        var dpi = GetDpiForWindow(hwnd);
+        float scalingFactor = (float)dpi / 96;
+        width = (int)(width * scalingFactor);
+        height = (int)(height * scalingFactor);
+
+        _ = SetWindowPos(hwnd, SpecialWindowHandles.HWND_TOP,
+                                    0, 0, width, height,
+                                    SetWindowPosFlags.SWP_NOMOVE);
+        _ = SetWindowLong(hwnd,
+               WindowLongIndexFlags.GWL_STYLE,
+               (SetWindowLongFlags)(GetWindowLong(hwnd,
+                  WindowLongIndexFlags.GWL_STYLE) &
+                  ~(int)SetWindowLongFlags.WS_MINIMIZEBOX &
+                  ~(int)SetWindowLongFlags.WS_MAXIMIZEBOX));
+    }
+
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
